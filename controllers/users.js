c55aa98ae2c343e9ecb18cf1a0bf3a90fd73c4c6
Alisa Krasnopolina <alisakras9@gmail.com>
2023-07-03 18:34:11 +0300

    refactor: change cors

--- controllers/users.js
+++ controllers/users.js
@@ -67,6 +67,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
           sameSite: true,
+          secure: true,
         })
         .send({ email });
     })
