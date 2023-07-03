c83aa842f6cfff56291111377708ca49db1c63f3
Alisa Krasnopolina <alisakras9@gmail.com>
2023-07-03 19:03:45 +0300

    refactor: change cors

--- controllers/users.js
+++ controllers/users.js
@@ -66,7 +66,7 @@ module.exports.login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          sameSite: none,
           secure: true,
         })
         .send({ email });
